7e0c7dc48c273668188b6c2776cec57a684fece1
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-12-04 11:42:49 +0100

    local working

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"os"
 
-"labix.org/v2/mgo"
-"labix.org/v2/mgo/bson"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
+type Decepticon struct {
+	Name string
+}
+
+func config(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		return "localhost"
+	}
+	return v
+}
+
 func main() {
-  session, err :=
+	session, err := mgo.Dial(config("WERCKER_MONGODB_HOST"))
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	conn := session.DB("test").C("decepticons")
+	err = conn.Insert(&Decepticon{"Shockwave"}, &Decepticon{"Starscream"})
+	if err != nil {
+		panic(err)
+	}
+
+	result := Decepticon{}
+	err = conn.Find(bson.M{"name": "Shockwave"}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Decepticon: ", result.Name)
 }
